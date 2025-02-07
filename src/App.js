946e5d84b88b6da08466f7ebdd417ca0514487c2
DTinh <duytinh10833@gmail.com>
2025-02-07 16:53:16 +0700

    start-project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="app-container">
+       <button className="btn btn-primary">
+        test
+       </button>
+    </div>
+  );
+}
+
+export default App;
